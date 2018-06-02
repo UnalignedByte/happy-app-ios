555cdf2f97cf7be067c437200087d7008a9fe8d9
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-06-02 20:22:01 +0100

    Initial project setup

---
+++ Happy App/Source/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Happy App
+//
+//  Created by Rafal Grodzinski on 26/04/2018.
+//  Copyright © 2018 UnalignedByte. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
