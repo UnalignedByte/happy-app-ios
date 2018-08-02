39cb8ec0ea6cd3f90f175c9c73a73409eca74892
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-08-02 20:03:21 +0100

    Added images for vote buttons

--- Happy App/Source/View/MainViewController.swift
+++ Happy App/Source/View/MainViewController.swift
@@ -14,6 +14,7 @@ class MainViewController: UIViewController {
     @IBOutlet private weak var resultsHintLabel: UILabel!
     @IBOutlet private weak var selectionAreaView: UIView!
     @IBOutlet private weak var resultsAreaView: UIView!
+    @IBOutlet private var voteButtons: [UIButton]!
     private let disposeBag = DisposeBag()
     var viewModel: MainViewModelProtocol?
 
@@ -34,6 +35,15 @@ class MainViewController: UIViewController {
         viewModel.selectionAreaOpacity
             .map { CGFloat($0) }
             .bind(to: selectionAreaView.rx.alpha).disposed(by: disposeBag)
+
+        //voteButtons.forEach {
+            //$0.layer.cornerRadius = $0.layer.frame.width/2.0
+            //$0.layer.borderWidth = 2.0
+            //$0.layer.borderColor = UIColor(white: 0.967, alpha: 1.0).cgColor
+            //$0.layer.shadowColor = UIColor.black.cgColor
+            //$0.layer.shadowOpacity = 0.6
+            //$0.layer.shadowRadius = 6.0
+        //}
     }
 
     private func setupResultsArea() {
