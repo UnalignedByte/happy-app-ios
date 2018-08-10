f28ee1d54dbba5ecbc414c05afc24d1da0764696
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-08-10 01:28:59 +0100

    App group has been used and widget reflects state of the submission

--- Happy App Widget/Source/TodayViewController.swift
+++ Happy App Widget/Source/TodayViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 import NotificationCenter
 
 class TodayViewController: UIViewController, NCWidgetProviding {
+    private static let kSubmissionDate = "submission_date"
+
     @IBOutlet private weak var titleLabel: UILabel!
+    @IBOutlet private var heightConstraint: NSLayoutConstraint!
+    private let userDefaults = UserDefaults(suiteName: "group.com.unalignedbyte.happyapp")!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = String.forTranslation(.titleBefore)
+        setupView()
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
+        setupView()
         completionHandler(NCUpdateResult.newData)
     }
 
@@ -25,5 +30,27 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         let happinessPercentage = sender.tag * 25
         guard let url = URL(string: "happyapp://happinessPercentage=\(happinessPercentage)") else { return }
         extensionContext?.open(url, completionHandler: nil)
+    }
+
+    private func setupView() {
+        if canVote {
+            titleLabel.text = String.forTranslation(.titleBefore)
+            heightConstraint.isActive = false
+        } else {
+            titleLabel.text = String.forTranslation(.titleAfter)
+            heightConstraint.isActive = true
+        }
+    }
+
+    var canVote: Bool {
+        guard let date = userDefaults.object(forKey: TodayViewController.kSubmissionDate) as? Date else {
+            return true
+        }
+
+        guard let morningDate = Calendar.current.date(bySettingHour: 0, minute: 0, second: 0, of: Date()) else {
+            return true
+        }
+        let result = date.compare(morningDate)
+        return result != .orderedDescending
     }
 }
